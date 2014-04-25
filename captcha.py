bfd179edbf0665df2d29fd28e39dc32a5a2b6bc1
haandol <ldg55d@gmail.com>
2014-04-25 15:06:23 +0900

    applied pep8

--- captcha.py
+++ captcha.py
@@ -1,9 +1,11 @@
-#coding: utf-8
+# coding: utf-8
 
 import os
-import tempfile
 import random
-import Image, ImageDraw, ImageFont
+import Image
+import ImageDraw
+import ImageFont
+from tempfile import NamedTemporaryFile
 
 PWD = os.getcwd()
 SEP = os.path.sep
@@ -21,20 +23,21 @@ B = 50
 
 
 def generate_captcha(PATH=PWD):
-    '''
-        Author:
-            haandol <ldg55d@gmail.com>
+    """Author:
+        haandol <ldg55d@gmail.com>
 
-        Parameters:
-            PATH - path to where captha image will be stored
+    Parameters:
+        PATH - path to where captha image will be stored
 
-        Description:
-            using PIL, generate captcha image and its value and return them.
-            generated captha image is not encrypted, this module simply generate a image for the random character value
-            if you want to encrypting captcha library, you should find other library like python-recaptcha or something.
-    '''
-    with tempfile.NamedTemporaryFile(suffix='.jpg', dir=PATH, delete=False) as fp:
-        im = Image.new("RGB", (WIDTH, HEIGHT), (R,G,B))
+    Description:
+        using PIL, generate captcha image and its value and return them.
+        generated captha image is not encrypted,
+        this module simply generate a image for the random character value
+        if you want to encrypting captcha library,
+        you should find other library like python-recaptcha or something.
+    """
+    with NamedTemporaryFile(suffix='.jpg', dir=PATH, delete=False) as fp:
+        im = Image.new("RGB", (WIDTH, HEIGHT), (R, G, B))
         draw = ImageDraw.Draw(im)
         font = ImageFont.truetype(FONT_FILE, FONT_SIZE, encoding='unic')
 
